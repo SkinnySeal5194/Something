b09f5989eb7a3d25f45a9ed5dea4bf6f92c8b85b
SkinnySeal5194 <166976359+SkinnySeal5194@users.noreply.github.com>
2024-04-14 20:15:32 +1000

    try

---
+++ Level2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Level2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+        int total = 0;
+
+        for (int gradeCounter = 1; gradeCounter <= 5; gradeCounter++)
+        {
+
+            Console.Write("Enter the grade: ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            total = total + grade;
+
+        }
+        Console.WriteLine("The average of the class is: " + total / 5);
+        Console.Read();
+    }
+}
+
